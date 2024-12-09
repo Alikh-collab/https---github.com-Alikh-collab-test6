90a5c4ad06b860e6688b05b2ea12ae4a606b4d76
Alikhan <chort445@gmail.com>
2024-12-09 16:55:57 +0300

    test

---
+++ script.js
@@ -0,0 +1,53 @@
+const School =[
+    name = "ACS",
+    schoolPhoneNumber = "+7 707 48 25 15",
+    students = [
+        {studentName: "Islam", top: 24/5, progress: 100, lessons: 2},
+        {studentName: "Agashbek", top: 24/4, progress: 80, lessons: 2},
+        {studentName: "Aizhamal", top: 24/3, progress: 74, lessons: 2},
+        {studentName: "Nurdaulet", top: 24/10, progress: 0, lessons: 2},
+        {studentName: "Daulet", top: 24/4, progress: 100, lessons: 2},
+        {studentName: "Aqsultan", top: 24/5, progress: 100, lessons: 2},
+    ]
+]
+
+
+// function changeSchoolName() {
+//     let newSchoolName = prompt("Jana mektep atyn engiz:");
+
+//     School.name = newSchoolName
+
+//     return `Mektep aty ${newSchoolName} bolyp austy`
+// }
+
+
+// function changePhoneNumber() {
+//     let newSchoolphone = +prompt("Jana mektepke jana nomer jaz:");
+
+//     School.schoolPhoneNumber = newSchoolphone;
+
+//     return `Mektep nomeri ${newSchoolphone} bolyp ozgerdi`
+// }
+
+
+
+// function listStudentNames() {
+//     const stuName = School.map((a, b) => stuName);
+
+//     alert(stuName.join("\n"));
+// }
+
+
+
+// function addStudent() {
+//     let newSName = prompt("Jana student atyn jaz:")
+//     let newSProgress = +prompt("Jana sudent Progresn jaz:")
+//     let newSTop = +prompt("Jana student tobyn jaz:")
+//     let newSLessons = +prompt("Jana Student sabagyn jaz:")
+
+//     School.push((School) =>  newSName, newSProgress, newSLessons, newSTop);
+
+    
+
+// }
+// console.log(addStudent());
